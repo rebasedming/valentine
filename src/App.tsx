ba87f52c616717a3cbc2095e591cbd4a4d676df8
Ming Ying <ming.ying.nyc@gmail.com>
2024-02-14 16:23:13 -0500

    Modified messages

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ export default function Page() {
       "Pretty please",
       "With a chocolate rice cake on top",
       "What about a matcha frostie",
-      "PLEASE POOKIE",
       "But :*(",
       "I am going to die",
       "Yep im dead",
@@ -38,7 +37,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!!))</div>
         </>
       ) : (
         <>
